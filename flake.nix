bebb1ca91116e3a777833510ec80e09da20a9f32
fujitani sora <fujitanisora0414@gmail.com>
2026-02-21 23:24:47 +0900

    use shared-flake-nix for common devShell configuration

--- flake.nix
+++ flake.nix
@@ -2,20 +2,30 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    shared-flake.url = "github:sorafujitani/shared-flake-nix";
   };
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      shared-flake,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = shared-flake.lib.mkDevShell {
+          inherit pkgs;
           name = "wez-kv";
-          packages = with pkgs; [
+          buildInputs = with pkgs; [
             go_1_26
             goreleaser
             gopls
           ];
         };
-      });
+      }
+    );
 }
